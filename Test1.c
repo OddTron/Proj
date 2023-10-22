5e3e4997b27fb40a27686c3ed981c83086bbc517
Anthony <AACastro@pm.me>
2023-10-21 20:11:13 -0700

    C Temp

--- Test1.c
+++ Test1.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 
 int main() {
-    printf("Test");
+    for(int f = 0; f <= 300; f+= 20){
+        int c;
+        c = 5*(f-32)/9;
+        printf("%d\t%d\n", f, c);
+    }
     return 0;
 }
